9b818b91afab324f6667b914bcf10f7b1a53684e
Helen Cook <helen.cook@gmail.com>
2019-04-29 09:14:15 +0100

    2019q1 update

--- COMMANDS.R
+++ COMMANDS.R
@@ -11,6 +11,9 @@ base_dir <- getwd()
 parsed_dir <- file.path(base_dir, "data", "parsed")
 plots_dir <- file.path(base_dir, "plots")
 
+# set timezone for posixct
+Sys.setenv(TZ='UTC') 
+
 # gather the data together
 
 migr <- read.csv(file.path(parsed_dir, "migraine_start_end"), header=F, sep="\t", colClasses=c("POSIXct", "integer"))
@@ -126,6 +129,8 @@ m2018q1 <- make_migraine_plot("2018-01-01", "2018-03-31")
 m2018q2 <- make_migraine_plot("2018-04-01", "2018-06-30")
 m2018q3 <- make_migraine_plot("2018-07-01", "2018-09-30")
 m2018q4 <- make_migraine_plot("2018-10-01", "2018-12-31")
+m2019q1 <- make_migraine_plot("2019-01-01", "2019-03-31")
+m2019q2 <- make_migraine_plot("2019-04-01", "2019-06-30")
 
 c2017q3 <- make_clue_plot(c, "2017-07-01", "2017-09-30", "tomato", "darkred", "")
 c2017q4 <- make_clue_plot(c, "2017-10-01", "2017-12-31", "tomato", "darkred", "")
@@ -133,6 +138,8 @@ c2018q1 <- make_clue_plot(c, "2018-01-01", "2018-03-31", "tomato", "darkred", ""
 c2018q2 <- make_clue_plot(c, "2018-04-01", "2018-06-30", "tomato", "darkred", "")
 c2018q3 <- make_clue_plot(c, "2018-07-01", "2018-09-30", "tomato", "darkred", "")
 c2018q4 <- make_clue_plot(c, "2018-10-01", "2018-12-31", "tomato", "darkred", "")
+c2019q1 <- make_clue_plot(c, "2019-01-01", "2019-03-31", "tomato", "darkred", "")
+c2019q2 <- make_clue_plot(c, "2019-04-01", "2019-06-30", "tomato", "darkred", "")
 
 a2017q3 <- make_other_plot(aura, "2017-07-01", "2017-09-30", "lightgreen", "darkgreen", "aura")
 a2017q4 <- make_other_plot(aura, "2017-10-01", "2017-12-31", "lightgreen", "darkgreen", "aura")
@@ -158,6 +165,8 @@ plot_grid(title_2017, NULL, NULL, NULL, NULL, NULL, NULL, c2017q3, m2017q3, NULL
 ggsave(file.path(plots_dir, "migraine-plot-2017.pdf"), width=190, height=277, units="mm") # fit on A4 with 10cm borders on all sides
 plot_grid(title_2018, c2018q1, m2018q1, NULL, c2018q2, m2018q2, NULL, c2018q3, m2018q3, NULL, c2018q4, m2018q4, align="v", nrow=14, rel_heights=heights)
 ggsave(file.path(plots_dir, "migraine-plot-2018.pdf"), width=190, height=277, units="mm") # fit on A4 with 10cm borders on all sides
+plot_grid(title_2019, c2019q1, m2019q1, NULL, c2019q2, m2019q2, NULL, NULL, NULL, NULL, NULL, NULL, align="v", nrow=14, rel_heights=heights)
+ggsave(file.path(plots_dir, "migraine-plot-2019.pdf"), width=190, height=277, units="mm") # fit on A4 with 10cm borders on all sides
 
 # detailed plots
 # TODO: make these more useful
